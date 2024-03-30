356164b26758b17d8eda3b90e3561b286e74005c
MuhammadAnasRazavi <muhammadanasrazavi@gmail.com>
2024-03-30 13:02:25 +0530

    Added a single compose card.

--- app/src/main/java/com/example/composequadrantapp/MainActivity.kt
+++ app/src/main/java/com/example/composequadrantapp/MainActivity.kt
@@ -3,13 +3,21 @@ package com.example.composequadrantapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.composequadrantapp.ui.theme.ComposeQuadrantAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -29,12 +37,29 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-
+@Composable
+fun ComposableCard(modifier: Modifier = Modifier) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier.padding(16.dp)
+    ) {
+        Text(
+            text = stringResource(id = R.string.text_composable_title),
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(bottom = 16.dp)
+        )
+        Text(
+            text = stringResource(id = R.string.text_composable_desc),
+            textAlign = TextAlign.Justify
+        )
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
 fun ComposeQuadrantPreview() {
     ComposeQuadrantAppTheme {
-
+        ComposableCard()
     }
 }
