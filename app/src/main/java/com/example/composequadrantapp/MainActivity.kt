651ed7120e721d4436547a4e0b8516c59faefa84
MuhammadAnasRazavi <muhammadanasrazavi@gmail.com>
2024-03-30 13:24:28 +0530

    Added four compose cards with background color.

--- app/src/main/java/com/example/composequadrantapp/MainActivity.kt
+++ app/src/main/java/com/example/composequadrantapp/MainActivity.kt
@@ -3,8 +3,10 @@ package com.example.composequadrantapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
@@ -13,6 +15,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -30,7 +33,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    ComposeQuadrant()
                 }
             }
         }
@@ -38,28 +41,74 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun ComposableCard(modifier: Modifier = Modifier) {
+fun ComposeCard(composeTitle: String, composeDesc: String, modifier: Modifier = Modifier) {
     Column(
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = modifier.padding(16.dp)
     ) {
         Text(
-            text = stringResource(id = R.string.text_composable_title),
+            text = composeTitle,
             fontWeight = FontWeight.Bold,
             modifier = Modifier.padding(bottom = 16.dp)
         )
         Text(
-            text = stringResource(id = R.string.text_composable_desc),
+            text = composeDesc,
             textAlign = TextAlign.Justify
         )
     }
 }
 
-@Preview(showBackground = true)
+@Composable
+fun ComposeQuadrant(modifier: Modifier = Modifier) {
+    Column(modifier = modifier) {
+        Row(
+            modifier = Modifier.weight(1f)
+        ) {
+            ComposeCard(
+                composeTitle = stringResource(id = R.string.text_composable_title),
+                composeDesc = stringResource(id = R.string.text_composable_desc),
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxSize()
+                    .background(color = Color(0xFFEADDFF))
+            )
+            ComposeCard(
+                composeTitle = stringResource(id = R.string.image_composable_title),
+                composeDesc = stringResource(id = R.string.image_composable_desc),
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxSize()
+                    .background(color = Color(0xFFD0BCFF))
+            )
+        }
+        Row(
+            modifier = Modifier.weight(1f)
+        ) {
+            ComposeCard(
+                composeTitle = stringResource(id = R.string.row_composable_title),
+                composeDesc = stringResource(id = R.string.row_composable_desc),
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxSize()
+                    .background(color = Color(0xFFB69DF8))
+            )
+            ComposeCard(
+                composeTitle = stringResource(id = R.string.column_composable_title),
+                composeDesc = stringResource(id = R.string.column_composable_desc),
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxSize()
+                    .background(color = Color(0xFFF6EDFF))
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun ComposeQuadrantPreview() {
     ComposeQuadrantAppTheme {
-        ComposableCard()
+        ComposeQuadrant()
     }
 }
